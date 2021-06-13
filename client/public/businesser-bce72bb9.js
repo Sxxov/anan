dc101f5b104b70f83cca6363d182e0b613a7a4a2
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-06-13 15:31:43 +0800

    Build + publish @ 06-13-2021 15:31:43

--- client/public/businesser-bce72bb9.js
+++ client/public/businesser-bce72bb9.js
@@ -1,4 +1,4 @@
-import{aj as e,ai as t,C as r,ag as n}from"./index-2b40a607.js";class ToastItem extends e{uid=String(Date.now());text="";level=t.INFO;duration=2e3}
+import{aj as e,ai as t,C as r,ag as n}from"./index-0a67abf5.js";class ToastItem extends e{uid=String(Date.now());text="";level=t.INFO;duration=2e3}
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
--- client/public/index.js
+++ client/public/index.js
@@ -1,1 +1,1 @@
-export{aq as default}from"./index-2b40a607.js";
+export{aq as default}from"./index-0a67abf5.js";
